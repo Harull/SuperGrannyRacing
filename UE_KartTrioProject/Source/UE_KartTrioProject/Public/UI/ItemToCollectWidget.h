f80db70ac21ddf17eb2aedbeedf8756bf5203a0d
Olivier <148540754+OlivierM98@users.noreply.github.com>
2025-02-24 17:04:00 +0100

    ui list WIP

---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/UI/ItemToCollectWidget.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ItemToCollectWidget.generated.h"
+
+UCLASS()
+class UE_KARTTRIOPROJECT_API UItemToCollectWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/UI/MainWidget.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainWidget.generated.h"
+
+UCLASS()
+class UE_KARTTRIOPROJECT_API UMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+	
+};
