fb07c331c8858978a14abd9e10d186c3683a3e7e
Olivier <148540754+OlivierM98@users.noreply.github.com>
2025-02-25 11:01:35 +0100

    GM

---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/GM_KartBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_KartBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_KARTTRIOPROJECT_API AGM_KartBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/UI/UsableItemWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UsableItemWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_KARTTRIOPROJECT_API UUsableItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
