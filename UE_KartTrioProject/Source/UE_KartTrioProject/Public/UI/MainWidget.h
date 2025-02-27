a2a2d2f637865add8b9685dd384c6e1807d4b690
Olivier <148540754+OlivierM98@users.noreply.github.com>
2025-02-27 13:59:49 +0100

    kmh widget

--- UE_KartTrioProject/Source/UE_KartTrioProject/Public/UI/MainWidget.h
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/UI/MainWidget.h
@@ -3,13 +3,19 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include <Components/TextBlock.h>
 #include "MainWidget.generated.h"
 
 UCLASS()
 class UE_KARTTRIOPROJECT_API UMainWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UTextBlock> speedDisplayer;
+	UPROPERTY() int speed;
 
+protected:
+	virtual void NativeConstruct() override;
 
+	void DebugSpeed();
 	
 };
