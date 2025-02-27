b50229e1d2b84934ee3da9f70cfc4f18a6bb1e9d
Olivier <148540754+OlivierM98@users.noreply.github.com>
2025-02-27 15:47:16 +0100

    lobby widget

---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/UI/SessionListWidget.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SessionListWidget.generated.h"
+
+UCLASS()
+class UE_KARTTRIOPROJECT_API USessionListWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
