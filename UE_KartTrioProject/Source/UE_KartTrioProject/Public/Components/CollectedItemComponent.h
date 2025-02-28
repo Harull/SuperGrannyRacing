9d78ac7703405c4ca959fdf5bf43217fb95c02a8
SilverIkito <148540610+SilverIkito@users.noreply.github.com>
2025-02-28 11:09:27 +0100

    Begin ComponentCollectedItem

---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/Components/CollectedItemComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include <GPE/CollectedItem.h>
+#include "CollectedItemComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UE_KARTTRIOPROJECT_API UCollectedItemComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	UPROPERTY(VisibleAnywhere) TArray<TObjectPtr<ACollectedItem>> listItem;
+	UPROPERTY(EditAnywhere) int nbItemCollected = 0;
+	UPROPERTY(EditAnywhere) bool canFinish = false;
+
+public:
+	TObjectPtr<ACollectedItem> GetCurrentItem() const { return listItem[nbItemCollected]; }
+
+public:	
+	UCollectedItemComponent();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
