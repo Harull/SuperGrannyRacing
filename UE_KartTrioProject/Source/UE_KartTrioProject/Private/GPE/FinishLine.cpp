35186c84c9750620210540b8077d9d4c488807a2
SilverIkito <148540610+SilverIkito@users.noreply.github.com>
2025-02-28 14:42:09 +0100

    Add FinishLine

---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Private/GPE/FinishLine.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GPE/FinishLine.h"
+#include "Components/BillboardComponent.h"
+#include <SeniorPlayer.h>
+#include <Components/CollectedItemComponent.h>
+#include <Kismet/KismetSystemLibrary.h>
+
+// Sets default values
+AFinishLine::AFinishLine()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+	billboard = CreateDefaultSubobject<UBillboardComponent>("Billboard");
+	box = CreateDefaultSubobject<UBoxComponent>("Box");
+
+	billboard->SetupAttachment(RootComponent);
+	box->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AFinishLine::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFinishLine::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFinishLine::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	ASeniorPlayer* _player = Cast<ASeniorPlayer>(OtherActor);
+	if (!_player) return;
+	UCollectedItemComponent* _comp =  _player->GetComponentByClass<UCollectedItemComponent>();
+	if (!_comp) return;
+	if (_comp->CanFinish())
+	{
+		UKismetSystemLibrary::PrintString(this, "Finish");
+	}
+	else
+	{
+
+		UKismetSystemLibrary::PrintString(this, "NO Finish");
+	}
+}
+
---
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Public/GPE/FinishLine.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Components/BoxComponent.h>
+#include "FinishLine.generated.h"
+
+UCLASS()
+class UE_KARTTRIOPROJECT_API AFinishLine : public AActor
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere) TObjectPtr<UBillboardComponent> billboard = nullptr;
+	UPROPERTY(EditAnywhere) TObjectPtr<UBoxComponent> box = nullptr;
+	
+public:	
+	AFinishLine();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+};
