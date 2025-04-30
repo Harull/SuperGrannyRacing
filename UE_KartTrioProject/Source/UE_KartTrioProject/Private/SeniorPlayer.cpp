7d201407bce96e12bdb4d9e113f43d5694b1fe5b
SilverIkito <148540610+SilverIkito@users.noreply.github.com>
2025-04-30 13:47:28 +0200

    Begin Widget

--- UE_KartTrioProject/Source/UE_KartTrioProject/Private/SeniorPlayer.cpp
+++ UE_KartTrioProject/Source/UE_KartTrioProject/Private/SeniorPlayer.cpp
@@ -12,6 +12,7 @@
 #include <GPE/ReplicatedStartManager.h>
 #include <Kismet/GameplayStatics.h>
 #include "Components/InventoryComponent.h"
+#include <GIS/WS_PlayerClassement.h>
 
 // Sets default values
 ASeniorPlayer::ASeniorPlayer()
@@ -72,6 +73,14 @@ void ASeniorPlayer::BeginPlay()
 	
 	FString _steamName = GetWorld()->GetGameInstance()->GetSubsystem<UGIS_Online>()->GetSteamUserName();
 	UKismetSystemLibrary::PrintString(this, _steamName); // TODO Remove
+
+	if (IsLocallyControlled())
+	{
+		UWS_PlayerClassement* _sub = GetWorld()->GetSubsystem<UWS_PlayerClassement>();
+		_sub->AddPlayerInMap(this, _steamName);
+	
+	}
+
 }
 
 void ASeniorPlayer::SendNotifyIsReady()
@@ -116,6 +125,8 @@ void ASeniorPlayer::InitSubsystem()
 		return;
 	}
 	_subsys->AddMappingContext(mapping, 0);
+
+	
 }
 
 void ASeniorPlayer::InitInputs(TObjectPtr<UEnhancedInputComponent> _inputComponent)
